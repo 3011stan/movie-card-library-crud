c17e858e6a50a8b88eb11e5a7ef8a56788819092
Stanrley Barbosa <3011stan@gmail.com>
2021-06-23 10:21:37 -0300

    Fix bug router in app

--- src/App.js
+++ src/App.js
@@ -11,21 +11,23 @@ import Copyright from './components/Copyright';
 
 function App() {
   return (
-    <Router>
+    <>
       <header className="container">
         <h1 className="display-2">Movie Card Library CRUD</h1>
       </header>
-      <Switch>
-        <Route path="/movies/:id/edit" component={ EditMovie } />
-        <Route path="/movies/new" component={ NewMovie } />
-        <Route exact path="/" component={ MovieList } />
-        <Route path="/movies/:id" component={ MovieDetails } />
-        <Route path="*" component={ NotFound } />
-      </Switch>
+      <Router>
+        <Switch>
+          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route path="/movies/new" component={ NewMovie } />
+          <Route exact path="/" component={ MovieList } />
+          <Route path="/movies/:id" component={ MovieDetails } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </Router>
       <footer className="footer-class text-muted">
         <Copyright />
       </footer>
-    </Router>
+    </>
   );
 }
 export default App;
