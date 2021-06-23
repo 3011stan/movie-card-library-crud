b51667759676c539767916109f972d93e99cda02
3011stan <49836837+3011stan@users.noreply.github.com>
2021-06-23 11:47:10 -0300

    Add files via upload

---
+++ src/pages/NewMovie.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router';
+
+import MovieForm from '../components/MovieForm';
+import * as movieAPI from '../services/movieAPI';
+
+class NewMovie extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      shouldRedirect: false,
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  async handleSubmit(newMovie) {
+    // req. API????
+    const { createMovie } = movieAPI;
+    const res = await createMovie(newMovie);
+    if (res) this.setState({ shouldRedirect: true });
+  }
+
+  render() {
+    const { shouldRedirect } = this.state;
+    if (shouldRedirect) return <Redirect to="/" />;
+    return (
+      <div data-testid="new-movie">
+        <MovieForm onSubmit={ this.handleSubmit } />
+      </div>
+    );
+  }
+}
+
+export default NewMovie;
