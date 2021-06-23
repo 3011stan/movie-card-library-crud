d35f7814956f510a83c17c1e858c78d51f5e7d35
Stanrley Barbosa <3011stan@gmail.com>
2021-06-23 01:59:49 -0300

    Updates

---
+++ precache-manifest.0d9cd01db605eda82bcbaf3f3ece3584.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0452b3736ede5f9a597ddd4c4fdd7050",
+    "url": "/movie-card-library-crud/index.html"
+  },
+  {
+    "revision": "68c557a4a7476d2fba58",
+    "url": "/movie-card-library-crud/static/css/main.cfde181c.chunk.css"
+  },
+  {
+    "revision": "63f2000784de12d4466b",
+    "url": "/movie-card-library-crud/static/js/2.452def6c.chunk.js"
+  },
+  {
+    "revision": "68c557a4a7476d2fba58",
+    "url": "/movie-card-library-crud/static/js/main.5d006a74.chunk.js"
+  },
+  {
+    "revision": "636dadd53749eb2456d4",
+    "url": "/movie-card-library-crud/static/js/runtime-main.7b328ff8.js"
+  }
+]);
